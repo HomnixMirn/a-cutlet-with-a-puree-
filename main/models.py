9aeca14ba988a223e8037aab1c84e8e03675cb02
Shadow7x <loginvlad1000@gmail.com>
2024-11-22 13:51:34 +0400

    db

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
 
 # Create your models here.
+
+class user (models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    fname = models.CharField(max_length=50)
+    email = models.EmailField()
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class token (Token):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from .models import *
 # from .serializers import *
 from django.http import HttpRequest
 from django.db.models import Q
@@ -9,3 +10,17 @@ from django.contrib.auth import authenticate
 import json
 # Create your views here.
 
+@api_view(['POST'])
+def register(request: HttpRequest):
+    try:
+        data = json.loads(request.body)
+        username = data['username']
+        password = data['password']
+        email = data['email']
+        created_user = User.objects.create_user(username=username, password=password)
+        user.objects.create(user=created_user, name=data['name'], fname=data['fname'], email=email)
+        
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
