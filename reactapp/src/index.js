81cbc19706e80b6c1f6c1da7cf56b75f012e1b41
ZikAsakur <khairutdinovraushan@gmail.com>
2024-11-24 01:54:08 +0300

    activ

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import {  BrowserRouter} from "react-router-dom";
 export const API_URL = "http://localhost:8000/api/calendar/"
-
+export const API_MEDIA = "http://localhost:8000"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
